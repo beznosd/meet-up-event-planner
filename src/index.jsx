1306af02f25531e8450c1689eddf1e13f142c09b
Dima Beznos <beznos.d@gmail.com>
2016-11-22 17:46:05 +0200

    Separated routes to file routes.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 import 'pickadate/lib/picker';
 import 'pickadate/lib/picker.date';
@@ -11,27 +11,9 @@ import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.min';
 import './css/style.css';
 
-import App from './components/App';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import EventsList from './components/EventsList';
-import CreateEvent from './components/CreateEvent';
-import Event from './components/Event';
-import NotFound from './components/NotFound';
+import routes from './routes';
 
 render(
-	<Router history={browserHistory}>
-		<Route path="/" component={App}>
-			<IndexRoute component={SignUp} />
-			<Route path="/login" component={Login} />
-			<Route path="/signup" component={SignUp} />
-			<Route path="/events">
-				<IndexRoute component={EventsList} />
-				<Route path=":eventId" component={Event} />
-			</Route>
-			<Route path="/create-event" component={CreateEvent} />
-			<Route path="*" component={NotFound} />
-		</Route>
-	</Router>,
+	<Router history={browserHistory} routes={routes} />,
 	document.getElementById('root')
 );
---
+++ src/routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import EventsList from './components/EventsList';
+import CreateEvent from './components/CreateEvent';
+import Event from './components/Event';
+import NotFound from './components/NotFound';
+
+export default (
+	<Route path="/" component={App}>
+		<IndexRoute component={SignUp} />
+		<Route path="/login" component={Login} />
+		<Route path="/signup" component={SignUp} />
+		<Route path="/events">
+			<IndexRoute component={EventsList} />
+			<Route path=":eventId" component={Event} />
+		</Route>
+		<Route path="/create-event" component={CreateEvent} />
+		<Route path="*" component={NotFound} />
+	</Route>
+);
